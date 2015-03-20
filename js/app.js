2271b38a8d7c86232d60e2b0b70f57d7f44a8c9c
bsoetarto7 <bsoetarto7@gmail.com>
2015-03-20 13:22:11 -0700

    added index and routing

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// Ng-routing to the pages when linked to
+var eCardApp = angular.module('eCardApp', [
+    'ngRoute']);
+
+
+timerApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/page1',{
+                // 
+                templateUrl:'home.html'
+            }).
+            when('/page2',{
+                // 
+                templateUrl:''
+            }).
+            when('/page3',{
+                // 
+                templateUrl:''
+            }).
+            otherwise({
+                redirectTo: '/page1'
+            });
+    }
+]);
